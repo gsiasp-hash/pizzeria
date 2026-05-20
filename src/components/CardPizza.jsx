ce067004e703b54101f036b3772c05c7ae9a8df8
guilleDev <g.siasp@gmail.com>
2026-05-20 12:56:43 -0400

    add footer

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 export default class CardPizza extends Component {
   render(props) {
     return (
-      <div className="card mt-4" style={{ width: "18rem" }}>
+      <div className="card my-5" style={{ width: "18rem" }}>
         <img src={this.props.img} className="card-img-top h-50" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">{this.props.name}</h5>
+          <h5 className="card-title fs-4">{this.props.name}</h5>
           <ul className="list-group list-group-flush">
             <li className="list-group-item d-flex flex-column text-center fw-bold">
               Ingredientes:
@@ -15,15 +15,15 @@ export default class CardPizza extends Component {
               </span>
             </li>
             <li className="list-group-item fw-bold text-center">
-              ${this.props.price.toLocaleString()}
+              Precio: ${this.props.price.toLocaleString()}
             </li>
           </ul>
           <div className="d-flex justify-content-around">
-            <button type="button" class="btn btn-dark">
-              Ver más
+            <button type="button" class="btn btn-light">
+              Ver más <i class="fa-solid fa-magnifying-glass"></i>
             </button>
             <button type="button" class="btn btn-dark">
-              Añadir
+              Añadir <i class="fa-solid fa-basket-shopping"></i>
             </button>
           </div>
         </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <div>Footer</div>
+      <div className='footer bg-dark text-white w-100'>
+        <p className='text-center py-3 fw-light'>© 2021 - Pizzería Mamma Mia!. Todos los derechos reservados.</p>
+      </div>
     )
   }
 }
