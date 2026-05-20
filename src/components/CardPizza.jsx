2419dce84f0c617ae7aa487b5d0ebf5db186e057
guilleDev <g.siasp@gmail.com>
2026-05-20 12:44:48 -0400

    fix imagenes de cards

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -4,21 +4,28 @@ export default class CardPizza extends Component {
   render(props) {
     return (
       <div className="card mt-4" style={{ width: "18rem" }}>
-        <img src={this.props.img} className="card-img-top" alt="..." />
+        <img src={this.props.img} className="card-img-top h-50" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{this.props.name}</h5>
-        </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">{this.props.ingredients.join(", ")}</li>
-          <li className="list-group-item">${this.props.price.toLocaleString()}</li>
-        </ul>
-        <div className="card-body">
-          <a href="#" className="card-link">
-            Card link
-          </a>
-          <a href="#" className="card-link">
-            Another link
-          </a>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item d-flex flex-column text-center fw-bold">
+              Ingredientes:
+              <span className="fw-light">
+                {this.props.ingredients.join(", ")}
+              </span>
+            </li>
+            <li className="list-group-item fw-bold text-center">
+              ${this.props.price.toLocaleString()}
+            </li>
+          </ul>
+          <div className="d-flex justify-content-around">
+            <button type="button" class="btn btn-dark">
+              Ver más
+            </button>
+            <button type="button" class="btn btn-dark">
+              Añadir
+            </button>
+          </div>
         </div>
       </div>
     );
