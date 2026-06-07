8077c8424f5052484cd24cfd5144ccba5ff9f837
Guillermo <g.siasp@gmail.com>
2026-06-07 19:23:05 -0400

    add lista de ingredientes

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class CardPizza extends Component {
   render(props) {
     return (
-      <div className="card" id={this.props.key} style={{ width: "18rem" }}>
+      <div className="card" style={{ width: "20rem", height: "100%" }}>
         <img
           src={this.props.img}
           className="card-img-top object-fit-cover"
@@ -13,32 +13,38 @@ export default class CardPizza extends Component {
         <div className="card-body">
           <h5 className="card-title fs-4 text-center">{this.props.name}</h5>
           <hr />
-          <div className="h-25">
-            <p className="list-group-item d-flex flex-column gap-1 text-center fw-bold">
-              Ingredientes:
+          <p className=" d-flex flex-column gap-1 text-center fw-bold">
+            Ingredientes:
+            <ul className="list-group list-group-flush">
               <span className="fw-light">
-                {this.props.ingredients.join(", ")}
+                {this.props.ingredients.map((ingredient) => (
+                  <li className="list-group-item" key={ingredient}>
+                    {ingredient}
+                  </li>
+                ))}
               </span>
-            </p>
-            <p className="list-group-item d-flex flex-column gap-1 text-center fw-bold">
-              Descripción:
-              <span className="fw-light">{this.props.desc}</span>
-            </p>
-          </div>
-          <hr />
-          <div>
-            <p className="list-group-item fw-bold text-center">
-              Precio: ${this.props.price.toLocaleString()}
-            </p>
+            </ul>
+          </p>
 
-            <div className="d-flex justify-content-around">
-              <button type="button" className="btn btn-light">
-                Ver más <i className="fa-solid fa-magnifying-glass"></i>
-              </button>
-              <button type="button" className="btn btn-dark">
-                Añadir <i className="fa-solid fa-basket-shopping"></i>
-              </button>
-            </div>
+          <p className=" d-flex flex-column gap-1 text-center fw-bold">
+            Descripción:
+            <span className="fw-light">{this.props.desc}</span>
+          </p>
+        </div>
+
+        <hr />
+        <div className="card-body d-flex flex-column gap-3">
+          <p className="list-group-item fw-bold text-center">
+            Precio: ${this.props.price.toLocaleString()}
+          </p>
+
+          <div className="d-flex justify-content-around">
+            <button type="button" className="btn btn-light">
+              Ver más <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+            <button type="button" className="btn btn-dark">
+              Añadir <i className="fa-solid fa-basket-shopping"></i>
+            </button>
           </div>
         </div>
       </div>
