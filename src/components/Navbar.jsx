9463d7f37defeb683e6d0de7d083b3511f498fe8
guilleDev <g.siasp@gmail.com>
2026-06-23 17:11:26 -0400

    add context total a navbar para actualizar carrito

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,72 +1,72 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router";
+import { useContext } from "react";
+import { TotalContext } from "../contexts/total.context";
 
-export default class Navbar extends Component {
-  render() {
-    const total = 25000;
-    const token = false;
-    return (
-      <nav className="navbar navbar-expand-lg py-1 bg-dark navbar-dark">
-        <div className="container-fluid">
-          <NavLink className="navbar-brand" to="/">
-            Pizzería Mamma Mia
-          </NavLink>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarText"
-            aria-controls="navbarText"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarText">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+export default function Navbar() {
+  const token = false;
+  const { total } = useContext(TotalContext);
+  return (
+    <nav className="navbar navbar-expand-lg py-1 bg-dark navbar-dark">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/">
+          Pizzería Mamma Mia
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="/">
+                Home
+              </NavLink>
+            </li>
+            {token && (
               <li className="nav-item">
-                <NavLink className="nav-link active" aria-current="page" to="/">
-                  Home
+                <NavLink className="nav-link" to="/profile">
+                  Profile
                 </NavLink>
               </li>
-              {token && (
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/profile">
-                    Profile
-                  </NavLink>
-                </li>
-              )}
-              {token && (
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/logout">
-                    Logout
-                  </NavLink>
-                </li>
-              )}
-              {!token && (
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/login">
-                    Login
-                  </NavLink>
-                </li>
-              )}
-              {!token && (
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/register">
-                    Register
-                  </NavLink>
-                </li>
-              )}
-            </ul>
-            <NavLink className="nav-link" to="/cart">
-              <span className="navbar-text border border-info rounded px-3 py-0 text-white fw-light btn">
-                <i className="fa-solid fa-basket-shopping"></i> Total: $
-                {total.toLocaleString()}
-              </span>
-            </NavLink>
-          </div>
+            )}
+            {token && (
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/logout">
+                  Logout
+                </NavLink>
+              </li>
+            )}
+            {!token && (
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">
+                  Login
+                </NavLink>
+              </li>
+            )}
+            {!token && (
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/register">
+                  Register
+                </NavLink>
+              </li>
+            )}
+          </ul>
+          <NavLink className="nav-link" to="/cart">
+            <span className="navbar-text border border-info rounded px-3 py-0 text-white fw-light btn">
+              <i className="fa-solid fa-basket-shopping"></i> Total: $
+              {total.toLocaleString()}
+            </span>
+          </NavLink>
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
