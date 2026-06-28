7d2173e2da5e94ea5199f527d8bd1f6743e12259
Guillermo <g.siasp@gmail.com>
2026-06-27 22:43:22 -0400

    Fix posicion footer en home

--- src/App.jsx
+++ src/App.jsx
@@ -16,22 +16,24 @@ import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <BrowserRouter>
-      <div className="d-flex flex-column min-vh-screen">
+      <div className="d-flex flex-column min-vh-100">
         <PizzasProvider>
           <TotalProvider>
             <Toaster position="bottom-right" reverseOrder={false} />
             <Navbar />
 
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/pizza/p001" element={<Pizza />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/404" element={<NotFound />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
+            <div className="flex-grow-1">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/pizza/p001" element={<Pizza />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/404" element={<NotFound />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
 
             <Footer />
           </TotalProvider>
