13ce10b6fd229067df6adce333a20617023ec7ac
Guillermo <g.siasp@gmail.com>
2026-07-12 16:25:38 -0400

    change: cambio de logica en user context para consumir la api de login y register

--- frontend/src/contexts/User.context.jsx
+++ frontend/src/contexts/User.context.jsx
@@ -1,13 +1,42 @@
 import { createContext, useState } from "react";
 
+const fetchLogin = async (username, password) => {
+  const response = await fetch("http://localhost:3000/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+  return response.json();
+};
+
+const fetchRegister = async (username, password) => {
+  const response = await fetch("http://localhost:3000/api/users/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+  return response.json();
+};
+
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
 
-  const login = () => {
+  const login = async (username, password) => {
+    const userData = await fetchLogin(username, password);
     setIsLoggedIn(true);
   };
+
+  const register = async (username, password) => {
+    const userData = await fetchRegister(username, password);
+    setIsLoggedIn(true);
+  };
+
   const logout = () => {
     setIsLoggedIn(false);
   };
