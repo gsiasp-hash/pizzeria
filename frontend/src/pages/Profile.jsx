fe73aafef08d7cfc1d61ae152f6cfe308e61b4a8
guilleDev <g.siasp@gmail.com>
2026-07-14 09:43:13 -0400

    add funcionalidad de cierre de sesion a la pagina profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
+import {useContext} from "react";
+import UserContext from "../contexts/User.context";
 
-export default class Profile extends Component {
-  render() {
-    return (
-      <div className="container py-5">
-        <h1 className="display-3 text-center">
-          Bienvenid@, danniella@gmail.com
-        </h1>
-        <p className="lead text-center">Este es tu perfil</p>
-        <button className="btn btn-primary d-block mx-auto">
-          Cerrar sesión
-        </button>
-      </div>
-    );
-  }
+export default function Profile() {
+  const { email, setIsLoggedIn } = useContext(UserContext);
+
+  return (
+    <div className="container py-5">
+      <h1 className="display-3 text-center">Bienvenid@, {email}</h1>
+      <p className="lead text-center">Este es tu perfil</p>
+      <button className="btn btn-primary d-block mx-auto"
+      onClick={() => setIsLoggedIn(false)}>Cerrar sesión</button>
+    </div>
+  );
 }
