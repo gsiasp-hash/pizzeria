c4209cc1ef278be396f98aedb4517a2d21488031
guilleDev <g.siasp@gmail.com>
2026-07-24 15:09:25 -0400

    Change importa api desde al variable de entorno

--- frontend/src/contexts/User.context.jsx
+++ frontend/src/contexts/User.context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = '${import.meta.env.VITE_API_BASE_URL}';
 
 const UserContext = createContext();
 
