9228fe60fd8107f96f87f7003b0faa2d1ca7d622
guilleDev <g.siasp@gmail.com>
2026-07-24 15:16:26 -0400

    fix auth

--- frontend/src/contexts/User.context.jsx
+++ frontend/src/contexts/User.context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 
-const API_BASE = '${import.meta.env.VITE_API_BASE_URL}';
+const API_BASE = import.meta.env.VITE_API_URL;
 
 const UserContext = createContext();
 
